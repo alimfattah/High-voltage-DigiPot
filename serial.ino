a08bdebc72500c9fb6583913aa19d94a55a4e4e8
Fattahul Alim Muhamad Abdi <140358363+alimfattah@users.noreply.github.com>
2023-08-01 16:05:12 +0700

    Update serial.ino

--- serial.ino
+++ serial.ino
@@ -1,5 +1,5 @@
 #include <SPI.h>
-//byte address=0x11;//B00010001 //untuk mcp
+//byte address=0x11;//B00010001 //command write mcp
 int CS= 10;
 int dataIn;
 
@@ -8,7 +8,6 @@ void setup()
   pinMode (CS, OUTPUT);
    SPI.begin();
    Serial.begin(115200);
-   digitalPotWrite(0x00);
 }
 void loop() {
 
@@ -16,10 +15,17 @@ if(Serial.available())
 {
   int dataIn = Serial.parseInt();
       if(dataIn!=0){
-      digitalPotWrite(dataIn); //rentang dari 1-255 posisi
+      digitalPotWrite(dataIn); //between 1-255 pos
       Serial.println(dataIn);
       }
-
+      if(dataIn==256){
+      digitalPotWrite(0);
+      Serial.println("0");
+      }
+      if(dataIn>256||dataIn<0){
+      digitalPotWrite(127);
+      Serial.println("127");
+      }
 }
 }
 
