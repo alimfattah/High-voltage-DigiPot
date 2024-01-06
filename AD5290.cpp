0cf2740e4c064becaa757d8f3d000d09402bc8f0
Fattahul Alim Muhamad Abdi <140358363+alimfattah@users.noreply.github.com>
2024-01-06 20:29:02 +0700

    Update AD5290.cpp

--- AD5290.cpp
+++ AD5290.cpp
@@ -3,7 +3,6 @@
 byte AD5290::WiperWrite( byte posWiper)
 {
     digitalWrite( __nCSPin, LOW );  // open the path of the spi communication
-    SPI.transfer( 0x00);          // The command for wiper set position
     SPI.transfer( posWiper);       // The write command expects a second byte which is the value for the wiper
     digitalWrite( __nCSPin, HIGH); //close the path of the spi communication
 
